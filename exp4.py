158559f6b9865ec1d32217c93b30b8f0163e33bf
achari <acharit105@gmail.com>
2026-08-14 21:11:18 +0530

    experiment4

---
+++ exp4.py
@@ -0,0 +1,51 @@
+class Person:
+
+    def __init__(self, name, age):
+
+        self.name = name
+
+        self.age = age
+
+    def displayData(self):
+
+        print("In parent class displayData method")
+
+        print("Name:", self.name)
+
+        print("Age:", self.age)
+
+        print("\n")
+
+
+class Employee(Person):
+
+    def __init__(self, name, age, id):
+
+        # Calling constructor of super class
+        super().__init__(name, age)
+
+
+        self.empId = id
+
+
+    def displayData(self):
+
+        print("In child class displayData method")
+
+        print("Name:", self.name)
+
+        print("Age:", self.age)
+
+        print("Employee ID:", self.empId)
+
+
+# Person class object
+person = Person("Karthik Shaurya", 26)
+
+person.displayData()
+
+
+# Employee class object
+emp = Employee("Karthik Shaurya", 26, "E317")
+
+emp.displayData()

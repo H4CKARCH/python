b08c40b97bf2bde35c04ebcca372df7607f5812a
achari <acharit105@gmail.com>
2026-08-14 20:46:44 +0530

    experiment3

---
+++ exp3.py
@@ -0,0 +1,23 @@
+class OverloadDemo:
+    # sum method with default as None for parameters
+    def sum(self, a=None, b=None, c=None):
+
+        # When three parameters are passed
+        if a is not None and b is not None and c is not None:
+            s = a + b + c
+            print("Sum = ", s)
+
+        # When two parameters are passed
+        elif a is not None and b is not None:
+            s = a + b
+            print("Sum = ", s)
+
+
+# Create object
+od = OverloadDemo()
+
+# Calling sum with two parameters
+od.sum(7, 8)
+
+# Calling sum with three parameters
+od.sum(7, 8, 9)
